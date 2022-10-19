990ed1d519cca2e2b2f17de9b6199e49ccf874ce
unknown <estarossa.nrz@gmail.com>
2022-10-19 22:08:01 +0800

    Home and About

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="margin: 20%;text-align: center">
+    <h1>Yokoso, Watashi no World</h1>
+    <p>Hello World</p>
+</body>
+</html>
