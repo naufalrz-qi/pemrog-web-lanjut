dd64e45cca03766366af94044045dd05a01039a7
unknown <estarossa.nrz@gmail.com>
2022-10-20 17:14:53 +0800

    Adding Bootstrap

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body style="margin: 20%;text-align: center">
-    <h1>About Me</h1>
-    <p> Nama : Naufal Rifqi Zuhrian <br>
-        NIM : 2001040002 <br>
-        Prodi : S1 RPL <br>
-        Semester : 5 <br>
-        Fakultas : Teknik dan Desain</p>
-</body>
-</html>
+@extends('layout.app')
+@section('content')
+<h1>Ini adalah halaman About</h1>
+@endsection
